94b2bfa5d3c6eadc8d17133f8c49c23f7d065c32
wenhuafeng <547566993@qq.com>
2023-05-04 17:48:16 +0800

    组合python编译脚本为一个文件

--- build.py
+++ build.py
@@ -2,8 +2,8 @@ import os
 import sys
 import shutil
 
-hex2bin = 'src/hex2bin.exe'
-mot2bin = 'src/mot2bin.exe'
+makefile_hex2bin = 'src/hex2bin.exe'
+makefile_mot2bin = 'src/mot2bin.exe'
 code_directory = 'src'
 out_directory = 'bin/Debug'
 
@@ -11,6 +11,9 @@ command = 'hex2bin.exe adc.hex'
 #command = 'hex2bin.exe -k 4 -f 0x08000010 -C 0x04C11DB7 0xFFFFFFFF TRUE TRUE 0xFFFFFFFF adc.hex'
 target_file = 'adc.bin'
 
+"""
+makefile build support define
+"""
 def build():
     os.system('make clean')
     os.system('make')
@@ -38,18 +41,15 @@ def checksum(file):
             checksum_value += ord(byte)
     return hex(checksum_value)[2:]
 
-def run_exe(para):
-    if para == 'y':
-        os.chdir(out_directory)
-        os.system(command)
-        cs = checksum(target_file)
-        print('\r')
-        print('checksum: 0x' + cs)
-        print(os.getcwd()) # 获得当前工作目录
-    else:
-        print('\r')
+def run_exe():
+    os.chdir(out_directory)
+    os.system(command)
+    cs = checksum(target_file)
+    print('\r')
+    print('checksum: 0x' + cs)
+    print(os.getcwd()) # 获得当前工作目录
 
-def main(para):
+def makefile_build():
     # 进入src目录
     os.chdir(code_directory)
     build()
@@ -59,8 +59,8 @@ def main(para):
     os.chdir(os.path.join(current_dir, os.pardir))
 
     # 复制exe文件
-    cp_build_file(hex2bin, out_directory)
-    cp_build_file(mot2bin, out_directory)
+    cp_build_file(makefile_hex2bin, out_directory)
+    cp_build_file(makefile_mot2bin, out_directory)
 
     # 进入src目录
     os.chdir(code_directory)
@@ -69,7 +69,47 @@ def main(para):
     # 切换到上级目录
     current_dir = os.path.abspath(__file__)
     os.chdir(os.path.join(current_dir, os.pardir))
-    run_exe(para)
+    run_exe()
+
+"""
+cmake build support define
+"""
+cmake_hex2bin = 'build/hex2bin.exe'
+cmake_mot2bin = 'build/mot2bin.exe'
+
+MKDIR_BUILD_DIR = 'mkdir build'
+BUILD_DIR       = './build'
+CMAKE_COMMAND   = 'cmake -G"MinGW Makefiles" ../'
+MAKE_CLEAN      = 'make clean'
+MAKE            = 'make -j8'
+
+def cbuild():
+    os.system(MKDIR_BUILD_DIR)
+    os.chdir(BUILD_DIR)
+    os.system(CMAKE_COMMAND)
+    os.system(MAKE_CLEAN)
+    os.system(MAKE)
+
+def cmake_build():
+    cbuild()
+
+    # 切换到上级目录
+    current_dir = os.path.abspath(__file__)
+    os.chdir(os.path.join(current_dir, os.pardir))
+
+    # 复制exe文件
+    cp_build_file(cmake_hex2bin, out_directory)
+    cp_build_file(cmake_mot2bin, out_directory)
+
+    run_exe()
+
+def main(para):
+    if para == 'makefile':
+        makefile_build()
+    elif para == 'cmake':
+        cmake_build()
+    else:
+        print('input para error!\n')
 
 if __name__ == "__main__":
     main(sys.argv[1])
